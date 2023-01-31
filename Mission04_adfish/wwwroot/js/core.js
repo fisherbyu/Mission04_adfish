25e31678f3b5eacfe28ce4b1fcf61f2789fc67fb
Andrew Fisher <adfish@byu.edu>
2023-01-31 00:12:01 -0700

    Update Style Stuff and Fix JS Calc Function

---
+++ Mission04_adfish/wwwroot/js/core.js
@@ -0,0 +1,23 @@
+﻿var totalGrade = "Hello";
+
+
+$('#calc_grade_submit').click(function () {
+
+    let assWeight = 0.50;
+    let gpWeight = 0.10;
+    let quizWeight = 0.10;
+    let midtermWeight = 0.10;
+    let finalWeight = 0.10;
+    let INTEXWeight = 0.10;
+
+    let INTEXGrade = parseFloat($('#intex_Input').val()) * INTEXWeight;
+    let finalGrade = parseFloat($('#final_Input').val()) * finalWeight;
+    let midtermGrade = parseFloat($('#midterm_Input').val()) * midtermWeight;
+    let quizGrade = parseFloat($('#quiz_Input').val()) * quizWeight;
+    let gpGrade = parseFloat($('#gp_Input').val()) * gpWeight;
+    let assGrade = parseFloat($('#assignments_Input').val()) * assWeight;
+
+    let totalGrade = INTEXGrade + finalGrade + midtermGrade + quizGrade + gpGrade + assGrade;
+
+    $('#output').text(totalGrade.toFixed(2) + "% is your Expected Final Grade");
+});
